4850cca7345f42033558a83b4c6ffe4e168efc80
daniil <bubnov.d.e@gmail.com>
2017-06-06 18:58:40 +0300

    add spring stuff

---
+++ src/main/java/com/db/prisma/droolspoc/Application.java
@@ -0,0 +1,18 @@
+package com.db.prisma.droolspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by daniil on 6/6/17.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
